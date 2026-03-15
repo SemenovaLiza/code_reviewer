364a20aef76afead8db859d78a6bd253d8d657f5
SemenovaLiza <thealeeeaaa23@gmail.com>
2026-03-15 18:24:17 -0400

    added preprocessing

--- code_reviewer/agent.py
+++ code_reviewer/agent.py
@@ -1,20 +1,10 @@
 from dotenv import load_dotenv
-from pydantic import BaseModel
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser
-from langchain.agents import create_tool_calling_agent, AgentExecutor
-
+from transformers import pipeline
 # Custom tools that we will use. These are pulled from our tools.py
 from tools import code_review
 
 load_dotenv()
 
-class AgentResponse(BaseModel):
-    ...
+pipe = pipeline("text-generation", model="openai-community/gpt2")
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-prompt = ChatPromptTemplate.from_messages([("system", """You're professional code reviewer. You have to highlight every antipattern or bug you find. If code clean you must say exactly 'no bugs'.""")])
-
-
-
+response = pipe("text")
